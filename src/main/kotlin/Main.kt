c327a2cbb77fcbf1f662eab4f5d7399c5cf81972
enzhdanova <listochek.katerinskiy@gmail.com>
2022-10-05 16:45:53 +0700

    refactoring

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -26,7 +26,7 @@ fun quickSort(array: IntArray, startIndex: Int, endIndex: Int): IntArray {
     var j = endIndex
 
     while (i < j) {
-        while (i <= indexMediana && array[i] <= mediana) {
+        while (i < indexMediana && array[i] <= mediana) {
             i++
         }
 
@@ -45,8 +45,8 @@ fun quickSort(array: IntArray, startIndex: Int, endIndex: Int): IntArray {
         quickSort(array, startIndex, indexMediana - 1)
     }
 
-    if (endIndex - indexMediana - 1 > 1) {
-        quickSort(array, indexMediana, endIndex - 1)
+    if (endIndex - indexMediana > 1) {
+        quickSort(array, indexMediana, endIndex)
     }
 
     return array
@@ -55,45 +55,85 @@ fun quickSort(array: IntArray, startIndex: Int, endIndex: Int): IntArray {
 
 fun mergeSort(array: IntArray, startIndex: Int, endIndex: Int): IntArray {
     if (startIndex >= endIndex)
-        return array
+        return intArrayOf(array[startIndex])
 
-    val mediana = (startIndex+endIndex + 1) / 2
+    val mediana = (startIndex + endIndex + 1) / 2
+    val res1 = mergeSort(array, startIndex, mediana - 1)
+    val res2 = mergeSort(array, mediana, endIndex)
 
-    mergeSort(array, startIndex, mediana-1)
-    mergeSort(array, mediana, endIndex)
-
-    val resultArray = merge(array, startIndex, mediana, endIndex)
-
+    val resultArray = merge(res1, res2)
     return resultArray
 }
 
-fun merge(array: IntArray, startIndex: Int, mediana: Int, endIndex: Int) : IntArray {
-    var i = startIndex
-    var j = mediana
+fun merge(array1: IntArray, array2: IntArray): IntArray {
+    var i = 0
+    var j = 0
+    val l1 = array1.size
+    val l2 = array2.size
     var index = 0
-    val arrayTmp = IntArray(endIndex - startIndex + 1)
-    while (i < mediana && j < endIndex) {
-        if (array[i] < array[j]) {
-            arrayTmp[index] = array[i]
+    val arrayTmp = IntArray(array1.size + array2.size)
+    while (i < l1 && j < l2) {
+        if (array1[i] < array2[j]) {
+            arrayTmp[index] = array1[i]
             i++
         } else {
-            arrayTmp[index] = array[j]
+            arrayTmp[index] = array2[j]
             j++
         }
         index++
     }
 
-    while (i < mediana) {
-        arrayTmp[index] = array[i]
+    while (i < l1) {
+        arrayTmp[index] = array1[i]
         index++
         i++
     }
 
-    while (j <= endIndex) {
-        arrayTmp[index] = array[j]
+    while (j < l2) {
+        arrayTmp[index] = array2[j]
         index++
         j++
     }
 
     return arrayTmp
 }
+
+
+fun getHeap(array: IntArray, heapLen : Int): IntArray {
+    val k = array.size / 2 - 1
+
+    var tmp: Int
+
+    for (i in k downTo 0) {
+        val leftLeaf = 2 * i + 1
+        val rightLeaf = 2 * i + 2
+        if (leftLeaf < heapLen  && array[i] < array[leftLeaf]) {
+            tmp = array[i]
+            array[i] = array[leftLeaf]
+            array[leftLeaf] = tmp
+        }
+
+        if (rightLeaf < heapLen  && array[i] < array[rightLeaf]) {
+            tmp = array[i]
+            array[i] = array[rightLeaf]
+            array[rightLeaf] = tmp
+        }
+    }
+
+    return array
+}
+
+fun heapSort(array: IntArray) : IntArray {
+
+    for (i in array.size - 1  downTo 1) {
+        getHeap(array, i + 1)
+
+        val tmp = array[0]
+        array[0] = array[i]
+        array[i] = tmp
+    }
+
+
+    return array
+}
+
--- src/test/kotlin/SortsTest.kt
+++ src/test/kotlin/SortsTest.kt
@@ -3,53 +3,121 @@ import org.junit.Test
 
 class SortsTest {
     @Test
-    fun bubbleSortsDown() {
+    fun bubbleSort() {
         val intArrayForSort = intArrayOf(5, 4, 3, 2, 1)
         val resultIntArray = intArrayOf(1, 2, 3, 4, 5)
 
+        val intArrayForSort2 = intArrayOf(4, 5, 3, 1, 2)
+        val resultIntArray2 = intArrayOf(1, 2, 3, 4, 5)
+
+        val intArrayForSort3 = intArrayOf(1, 1, 1, 1, 1)
+        val resultIntArray3 = intArrayOf(1, 1, 1, 1, 1)
+
         val result = sortBubble(intArrayForSort)
-        val resultQuickSort = quickSort(intArrayForSort, 0, intArrayForSort.size - 1)
-        val resultMergeSort = mergeSort(intArrayForSort, 0, intArrayForSort.size - 1)
+        val result2 = sortBubble(intArrayForSort2)
+        val result3 = sortBubble(intArrayForSort3)
 
         assertArrayEquals(resultIntArray, result)
-        assertArrayEquals(resultIntArray, resultQuickSort)
-        assertArrayEquals(resultIntArray, resultMergeSort)
+        assertArrayEquals(resultIntArray2, result2)
+        assertArrayEquals(resultIntArray3, result3)
     }
 
     @Test
-    fun bubbleSorts() {
-        val intArrayForSort = intArrayOf(4, 5, 3, 1, 2)
+    fun quickSort() {
+        val intArrayForSort = intArrayOf(5, 4, 3, 2, 1)
         val resultIntArray = intArrayOf(1, 2, 3, 4, 5)
 
-        val result = sortBubble(intArrayForSort)
-        val resultQuickSort = quickSort(intArrayForSort, 0, intArrayForSort.size - 1)
-        val resultMergeSort = mergeSort(intArrayForSort, 0, intArrayForSort.size - 1)
+        val intArrayForSort2 = intArrayOf(4, 5, 3, 1, 2)
+        val resultIntArray2 = intArrayOf(1, 2, 3, 4, 5)
+
+        val intArrayForSort3 = intArrayOf(1, 1, 1, 1, 1)
+        val resultIntArray3 = intArrayOf(1, 1, 1, 1, 1)
+
+        val result = quickSort(intArrayForSort, 0, intArrayForSort.size - 1)
+        val result2 = quickSort(intArrayForSort2, 0, intArrayForSort2.size - 1)
+        val result3 = quickSort(intArrayForSort3, 0, intArrayForSort3.size - 1)
 
         assertArrayEquals(resultIntArray, result)
-        assertArrayEquals(resultIntArray, resultQuickSort)
-        assertArrayEquals(resultIntArray, resultMergeSort)
+        assertArrayEquals(resultIntArray2, result2)
+        assertArrayEquals(resultIntArray3, result3)
     }
 
     @Test
-    fun bubbleSorts2() {
-        val intArrayForSort = intArrayOf(1, 1, 1, 1, 1)
-        val resultIntArray = intArrayOf(1, 1, 1, 1, 1)
-
-        val result = sortBubble(intArrayForSort)
-        val resultQuickSort = quickSort(intArrayForSort, 0, intArrayForSort.size - 1)
-        val resultMergeSort = mergeSort(intArrayForSort, 0, intArrayForSort.size - 1)
-
-        assertArrayEquals(resultIntArray, result)
-        assertArrayEquals(resultIntArray, resultQuickSort)
-        assertArrayEquals(resultIntArray, resultMergeSort)
-    }
-
-    @Test
-    fun mergeSortedInt(){
-        val intArrayForSort = intArrayOf(1, 2, 3, 4, 5)
+    fun mergeSort() {
+        val intArrayForSort = intArrayOf(5, 4, 3, 2, 1)
         val resultIntArray = intArrayOf(1, 2, 3, 4, 5)
 
-        val resultMerge = merge(intArrayForSort, 0, intArrayForSort.size / 2, intArrayForSort.size - 1)
-        assertArrayEquals(resultIntArray, resultMerge)
+        val intArrayForSort2 = intArrayOf(4, 5, 3, 1, 2)
+        val resultIntArray2 = intArrayOf(1, 2, 3, 4, 5)
+
+        val intArrayForSort3 = intArrayOf(1, 1, 1, 1, 1)
+        val resultIntArray3 = intArrayOf(1, 1, 1, 1, 1)
+
+        val result = mergeSort(intArrayForSort, 0, intArrayForSort.size - 1)
+        val result2 = mergeSort(intArrayForSort2, 0, intArrayForSort2.size - 1)
+        val result3 = mergeSort(intArrayForSort3, 0, intArrayForSort3.size - 1)
+
+        assertArrayEquals(resultIntArray, result)
+        assertArrayEquals(resultIntArray2, result2)
+        assertArrayEquals(resultIntArray3, result3)
+    }
+
+    @Test
+    fun heapSort() {
+        val intArrayForSort = intArrayOf(5, 4, 3, 2, 1)
+        val resultIntArray = intArrayOf(1, 2, 3, 4, 5)
+
+        val intArrayForSort2 = intArrayOf(4, 5, 3, 1, 2)
+        val resultIntArray2 = intArrayOf(1, 2, 3, 4, 5)
+
+        val intArrayForSort3 = intArrayOf(1, 1, 1, 1, 1)
+        val resultIntArray3 = intArrayOf(1, 1, 1, 1, 1)
+
+        val result = heapSort(intArrayForSort)
+        val result2 = heapSort(intArrayForSort2)
+        val result3 = heapSort(intArrayForSort3)
+
+        assertArrayEquals(resultIntArray, result)
+        assertArrayEquals(resultIntArray2, result2)
+        assertArrayEquals(resultIntArray3, result3)
+    }
+
+    @Test
+    fun mergeSortedArray(){
+        val intArrayForSort1 = intArrayOf(5)
+        val intArrayForSort12 = intArrayOf(4)
+        val resultIntArray1 = intArrayOf(4, 5)
+
+        val resultMerge1 = merge(intArrayForSort1, intArrayForSort12)
+        assertArrayEquals(resultIntArray1, resultMerge1)
+
+        val intArrayForSort2 = intArrayOf(4, 5)
+        val intArrayForSort22 = intArrayOf(1, 2)
+        val resultIntArray2 = intArrayOf(1, 2, 4, 5)
+
+        val resultMerge2 = merge(intArrayForSort2, intArrayForSort22)
+        assertArrayEquals(resultIntArray2, resultMerge2)
+
+        val intArrayForSort3 = intArrayOf(1, 2)
+        val intArrayForSort32 = intArrayOf(3, 4, 5)
+        val resultIntArray3 = intArrayOf(1, 2, 3, 4, 5)
+
+        val resultMerge3 = merge(intArrayForSort3, intArrayForSort32)
+        assertArrayEquals(resultIntArray3, resultMerge3)
+    }
+
+    @Test
+    fun getHeap(){
+        val intArrayForSort = intArrayOf(24, 31, 15, 20,52, 6)
+        val resultIntArray = intArrayOf(52, 24, 15, 20, 31, 6)
+
+        val resultHeap = getHeap(intArrayForSort, intArrayForSort.size)
+        assertArrayEquals(resultIntArray, resultHeap)
+
+        val intArrayForSort2 = intArrayOf(5, 4, 3, 2, 1)
+        val resultIntArray2 = intArrayOf(5, 4, 3, 2, 1)
+
+        val resultHeap2 = getHeap(intArrayForSort2, intArrayForSort.size)
+        assertArrayEquals(resultIntArray2, resultHeap2)
     }
 }
